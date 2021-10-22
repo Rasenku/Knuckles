5e0f7f920486fd29fd5e2d3c3b9c11afaad8c59d
Anthony <sean.a.protho1@gmail.com>
2021-10-22 14:52:21 -0700

    trying toset up flask

---
+++ app.py
@@ -0,0 +1,287 @@
+"""Flask stuff"""
+from flask import Flask, request, render_template #imports pip3 libraries
+from random import sample, choice
+import unittest
+
+app = Flask(__name__) #defines server name
+
+class tests(unittest.TestCase):
+    def tests_index_returns_template_index(self):
+        self.assertEqual(index(), render_template("index.html"))
+
+@app.route('/')
+def index(): #creates a html section inside of python3's code
+    return render_template("index.html")
+
+if __name__ == "__main__": #closes server runtime
+    unittest.main() #runs tests
+    app.run(debug=True)
+
+"""P5 stuff"""
+# from p5 import *
+
+# bar_width = 20
+# last_bar = None
+
+# def setup():
+#     size(640, 360)
+#     title("Brightness")
+#     color_mode('HSB', width, 100, height)
+#     no_stroke()
+#     background(0)
+
+# def draw():
+#     global last_bar
+#     which_bar = mouse_x // bar_width
+#     if which_bar is not last_bar:
+#         bar_x = which_bar * bar_width
+#         fill(bar_x, 100, mouse_y)
+#         rect((bar_x, 0), bar_width, height)
+#         last_bar = which_bar
+
+# if __name__ == '__main__':
+#     run()
+
+"""Pygame/ Tkinter stuff"""
+# # makes our pygame display the same size as our grid + 8 for 8bit
+# screen = pygame.display.set_mode((), 8)
+# screen.set_alpha(None)
+
+# # sets our frame rate. Lower frame rate means faster loading times
+# FPS = 12
+# fpsClock = pygame.time.Clock()
+
+# # class for end and start points
+# class spot:
+#     def __init__(self, x, y):
+#         self.i = x
+#         self.j = y
+#         self.f = 0
+#         self.g = 0
+#         self.h = 0
+#         self.neighbors = []
+#         self.previous = None
+#         self.obs = False
+#         self.closed = False
+#         self.value = 1
+
+#     def show(self, color, st):
+#         if self.closed == False:
+#             pygame.draw.rect(screen, color, (self.i * w, self.j * h, w, h), st)
+#             pygame.display.update()
+
+#     def path(self, color, st):
+#         pygame.draw.rect(screen, color, (self.i * w, self.j * h, w, h), st)
+#         pygame.display.update()
+
+#     def addNeighbors(self, grid):
+#         i = self.i
+#         j = self.j
+#         if i < cols-1 and grid[self.i + 1][j].obs == False:
+#             self.neighbors.append(grid[self.i + 1][j])
+#         if i > 0 and grid[self.i - 1][j].obs == False:
+#             self.neighbors.append(grid[self.i - 1][j])
+#         if j < row-1 and grid[self.i][j + 1].obs == False:
+#             self.neighbors.append(grid[self.i][j + 1])
+#         if j > 0 and grid[self.i][j - 1].obs == False:
+#             self.neighbors.append(grid[self.i][j - 1])
+
+
+# cols = 50
+# grid = [0 for i in range(cols)]
+# row = 50
+# openSet = []
+# closedSet = []
+# red = (255, 0, 0)
+# green = (0, 255, 0)
+# blue = (0, 0, 255)
+# grey = (220, 220, 220)
+# w = 800 / cols
+# h = 800 / row
+# cameFrom = []
+
+# # create 2d array
+# for i in range(cols):
+#     grid[i] = [0 for i in range(row)]
+
+# # Create Spots
+# for i in range(cols):
+#     for j in range(row):
+#         grid[i][j] = spot(i, j)
+
+# # Set start and end node
+# start = grid[12][5]
+# end = grid[3][6]
+
+# # creates our rectangles
+# for i in range(cols):
+#     for j in range(row):
+#         grid[i][j].show((255, 0, 255), 1)
+
+# for i in range(0,row):
+#     grid[0][i].show(grey, 0)
+#     grid[0][i].obs = True
+#     grid[cols-1][i].obs = True
+#     grid[cols-1][i].show(grey, 0)
+#     grid[i][row-1].show(grey, 0)
+#     grid[i][0].show(grey, 0)
+#     grid[i][0].obs = True
+#     grid[i][row-1].obs = True
+
+# def onsubmit():
+#     global start
+#     global end
+#     st = startBox.get().split(',')
+#     ed = endBox.get().split(',')
+#     start = grid[int(st[0])][int(st[1])]
+#     end = grid[int(ed[0])][int(ed[1])]
+#     window.quit()
+#     window.destroy()
+
+
+# window = Toplevel()
+# window.title("Knuckles")
+# label = Label(window, text='Start(x,y): ')
+# startBox = Entry(window)
+# label1 = Label(window, text='End(x,y): ')
+# endBox = Entry(window)
+# var = IntVar()
+# showPath = ttk.Checkbutton(window, text='Show Steps :', onvalue=1, offvalue=0, variable=var)
+# submit = Button(window, text='Submit', command=onsubmit)
+
+# showPath.grid(columnspan=2, row=2)
+# submit.grid(columnspan=2, row=3)
+# label1.grid(row=1, pady=3)
+# endBox.grid(row=1, column=1, pady=3)
+# startBox.grid(row=0, column=1, pady=3)
+# label.grid(row=0, pady=3)
+
+# window.update()
+# mainloop()
+
+# pygame.init()
+# openSet.append(start)
+
+# # draws our obsticles
+# def mousePress(x):
+#     t = x[0]
+#     w = x[1]
+#     g1 = t // (800 // cols)
+#     g2 = w // (800 // row)
+#     acess = grid[g1][g2]
+#     if acess != start and acess != end:
+#         if acess.obs == False:
+#             acess.obs = True
+#             acess.show((255, 255, 255), 0)
+#             time.sleep(0.05) # Makes drawing more bearable
+#         # erased obsticles, but causes bugs
+#         elif acess.obs == True:
+#             acess.obs = False
+#             acess.show((0, 0, 0), 0)
+#             acess.show((255, 0, 255), 1)
+
+# # sets our points colors
+# end.show((255, 8, 127), 0)
+# start.show((255, 8, 127), 0)
+
+# loop = True
+# while loop:
+#     ev = pygame.event.get()
+
+#     for event in ev:
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#         if pygame.mouse.get_pressed()[0]:
+#             try:
+#                 pos = pygame.mouse.get_pos()
+#                 mousePress(pos)
+#             except AttributeError:
+#                 pass
+#         elif event.type == pygame.KEYDOWN:
+#             if event.key == pygame.K_SPACE:
+#                 loop = False
+#                 break
+
+# for i in range(cols):
+#     for j in range(row):
+#         grid[i][j].addNeighbors(grid)
+
+# # heurisitic: enabling a person to discover or learn something for themselves.
+# def heurisitic(n, e):
+#     # two operations that help our start point find our end point
+#     # d = abs(n.i - e.i) + abs(n.j - e.j)
+#     # this one is just faster
+#     d = math.sqrt((n.i - e.i)**2 + (n.j - e.j)**2)
+#     return d
+
+
+# def main(checked):
+#     if len(openSet) > 0:
+#         lowestIndex = 0
+#         for i in range(len(openSet)):
+#             if openSet[i].f < openSet[lowestIndex].f:
+#                 lowestIndex = i
+
+#         current = openSet[lowestIndex]
+#         if current == end:
+#             print('done', current.f)
+#             start.show((255,8,127),0)
+#             temp = current.f
+#             for i in range(round(current.f)):
+#                 current.closed = False
+#                 current.show((0,0,255), 0)
+#                 current = current.previous
+#             end.show((255, 8, 127), 0)
+
+#             Tk().wm_withdraw()
+#             result = messagebox.askokcancel('Program Finished', ('The program finished, the shortest distance \n to the path is ' + str(temp) + ' blocks away, \n would you like to re run the program?'))
+#             if result == True:
+#                 os.execl(sys.executable,sys.executable, *sys.argv)
+#             else:
+#                 ag = True
+#                 while ag:
+#                     ev = pygame.event.get()
+#                     for event in ev:
+#                         if event.type == pygame.KEYDOWN:
+#                             ag = False
+#                             break
+#             pygame.quit()
+
+#         openSet.pop(lowestIndex)
+#         closedSet.append(current)
+
+#         neighbors = current.neighbors
+#         for i in range(len(neighbors)):
+#             neighbor = neighbors[i]
+#             if neighbor not in closedSet:
+#                 tempG = current.g + current.value
+#                 if neighbor in openSet:
+#                     if neighbor.g > tempG:
+#                         neighbor.g = tempG
+#                 else:
+#                     neighbor.g = tempG
+#                     openSet.append(neighbor)
+
+#             neighbor.h = heurisitic(neighbor, end)
+#             neighbor.f = neighbor.g + neighbor.h
+
+#             if neighbor.previous == None:
+#                 neighbor.previous = current
+#     # if checkbox is checked
+#     if checked == 1:
+#         # show attempted paths
+#         for i in range(len(openSet)):
+#             openSet[i].show(green, 0)
+
+#         for i in range(len(closedSet)):
+#             if closedSet[i] != start:
+#                 closedSet[i].show(red, 0)
+#     current.closed = True
+
+# while True:
+#     ev = pygame.event.poll()
+#     if ev.type == pygame.QUIT:
+#         pygame.quit()
+#     pygame.display.update()
+#     main(var.get())
+#     fpsClock.tick(FPS)
